85716c96ec2c3dfcb15735c624f3c49a261a9320
sarexRGB <velez99sara0106@gmail.com>
2025-08-19 09:03:01 -0600

    inicio con js

---
+++ src/js/tic-tac-toe.js
@@ -0,0 +1,16 @@
+const q = document.getElementsByClassName("q");
+const statusText = document.getElementById("statusText");
+const restartBtn = document.getElementById("restartBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
